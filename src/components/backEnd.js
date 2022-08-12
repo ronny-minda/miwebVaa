3131fcd112000471cceb228e0050e93a14155438
ronny-minda <ronny.michael.minda.vera@gmail.com>
2022-08-12 13:26:58 -0500

    listo

--- src/components/backEnd.js
+++ src/components/backEnd.js
@@ -1,264 +1,269 @@
-import React, {useState} from "react";
-import styled from "styled-components";
+import React, { useState } from "react"
+import styled from "styled-components"
 import { StaticImage } from "gatsby-plugin-image"
-
+import img from "../images/imgData/0.png"
+import { AnimatePresence, motion } from "framer-motion"
 
 const Section = styled.section`
-    /* background-color: red; */
-    margin: 150px 0;
+  /* background-color: red; */
+  margin: 150px 0;
 
-    
-    h3 {
-        color: #fff;
-        font-size: 25px;
-        padding-left: 15px;
-    }
+  h3 {
+    color: #fff;
+    font-size: 25px;
+    padding-left: 15px;
+  }
 
-    .conteFron {
-        height: 500px;
-        width: auto;
-        /* background-color: #c1bbd1; */
-        /* display: flex;
+  .conteFron {
+    height: 500px;
+    width: auto;
+    /* background-color: #c1bbd1; */
+    /* display: flex;
         justify-content: center;
         align-items: center; */
-        position: relative;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-        .verCodigo {
-            backdrop-filter: blur(5px);
-            position: absolute;
-            bottom: 50px;
-            padding: 10px 20px;
-            border-radius: 3px;
-            border: 2px solid #fff;
-            color: #fff;
-            background-color: #0009;
-        }
-        .verCodigo:active {
-            background-color: #c1bbd1;
-        }
-        
-        .cart {
-            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-            /* background-color: aquamarine; */
-            height: auto;
-            width: 90%;
-            max-width: 1000px;
-
-            .imgI {
-            }
-        }
-        .botonDere {
-            height: 50px;
-            width: 50px;
-            background-color: #000;
-            position: absolute;
-            top: 50%;
-            right: 10px;
-        }
-        .botonIz {
-            height: 50px;
-            width: 50px;
-            background-color: #000;
-            position: absolute;
-            top: 50%;
-            left: 10px;
-        }
+    .verCodigo {
+      backdrop-filter: blur(5px);
+      position: absolute;
+      bottom: 50px;
+      padding: 10px 20px;
+      border-radius: 3px;
+      border: 2px solid #fff;
+      color: #fff;
+      background-color: #0009;
     }
-`;
+    .verCodigo:active {
+      background-color: #c1bbd1;
+    }
+
+    .cart {
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+      /* background-color: aquamarine; */
+      height: auto;
+      width: 90%;
+      max-width: 1000px;
+
+      .imgI {
+      }
+    }
+    .botonDere {
+      height: 50px;
+      width: 50px;
+      background-color: #000;
+      position: absolute;
+      top: 50%;
+      right: 10px;
+    }
+    .botonIz {
+      height: 50px;
+      width: 50px;
+      background-color: #000;
+      position: absolute;
+      top: 50%;
+      left: 10px;
+    }
+  }
+`
 
 const BackEnd = () => {
+  const [imgesta, setImgesta] = useState(0)
 
-    
+  console.log(imgesta)
 
-    const [ imgesta, setImgesta ] = useState(0);
+  return (
+    <Section>
+      <AnimatePresence>
+        {false && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            asdfssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-    console.log(imgesta)
-
-    
-
-    return (
-        <Section>
-            <h3>Proyectos del lado del Frontend</h3>
-            <div className="conteFron">
-
-                {/* <div className="cart">
+      <h3>Proyectos del lado del Frontend</h3>
+      <div className="conteFron">
+        {/* <div className="cart">
 
                 </div> */}
-                
-                {
-                    imgesta == 0 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/0.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 1 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/1.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 2 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/2.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 3 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/3.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
 
+        {imgesta == 0 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/0.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 1 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/1.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 2 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/2.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 3 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/3.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
 
+        {imgesta == 4 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/4.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 5 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/5.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 6 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/6.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 7 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/7.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 8 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/8.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 9 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/9.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
+        {imgesta == 10 ? (
+          <div className="cart">
+            <StaticImage
+              class="imgI"
+              src={`../images/imgData/10.png`}
+              alt="A dinosaur"
+              placeholder="blurred"
+              layout="fullWidth"
+              objectFit="fill"
+              //    width={1000}
+              //    height={400}
+            />
+          </div>
+        ) : null}
 
+        <div className="verCodigo">ver codigo</div>
 
-{
-                    imgesta == 4 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/4.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 5 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/5.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 6 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/6.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 7 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/7.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 8 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/8.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 9 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/9.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-                {
-                    imgesta == 10 ? (<div className="cart">
-                    <StaticImage
-                        class="imgI"
-                        src={`../images/imgData/10.png`}
-                        alt="A dinosaur"
-                        placeholder="blurred"
-                        layout="fullWidth"
-                        objectFit="fill"
-                    //    width={1000}
-                    //    height={400}
-                    />
-                </div>) : null
-                }
-
-                <div className="verCodigo">ver codigo</div>
-                
-
-                
-
-                <div onClick={ () => imgesta > 0 ? setImgesta(imgesta - 1) : null } className="botonIz"></div>
-                <div onClick={ () => imgesta < 10 ? setImgesta(imgesta + 1) : null } className="botonDere"></div>
-                
-            </div>
-        </Section>
-    )
-
+        <div
+          onClick={() => (imgesta > 0 ? setImgesta(imgesta - 1) : null)}
+          className="botonIz"
+        ></div>
+        <div
+          onClick={() => (imgesta < 10 ? setImgesta(imgesta + 1) : null)}
+          className="botonDere"
+        ></div>
+      </div>
+    </Section>
+  )
 }
 
 export default BackEnd
--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React from "react"
 
-import styled from "styled-components";
-
+import styled from "styled-components"
+import { motion } from "framer-motion"
 
 const Form = styled.form`
+  .divCont {
     display: flex;
     flex-direction: column;
     margin: 120px 0;
@@ -11,109 +12,114 @@ const Form = styled.form`
     margin: auto;
 
     @media (max-width: 1000px) {
-        width: 90%;
+      width: 90%;
     }
 
     .sutmit {
-        width: 70%;
-        margin: auto;
-        outline: none;
-        height: 50px;
-        background-color: #fff3;
-        color: #fff;
-        border: 3px solid #fff9;
+      width: 70%;
+      margin: auto;
+      outline: none;
+      height: 50px;
+      background-color: #fff3;
+      color: #fff;
+      border: 3px solid #fff9;
     }
     .sutmit:active {
-        background-color: #fff5;
-        border: 3px solid #fff;
+      background-color: #fff5;
+      border: 3px solid #fff;
     }
 
     label {
-        display: flex;
-        flex-direction: column;
-        textarea {
-            outline: none;
-            height: 150px;
-            background-color: #fff0;
-            color: #fff;
-            border: 3px solid #fff9;
-            margin: 10px;
-        }
-        textarea:focus {
-            border: 3px solid #fff;
-        }
+      display: flex;
+      flex-direction: column;
+      textarea {
+        outline: none;
+        height: 150px;
+        background-color: #fff0;
+        color: #fff;
+        border: 3px solid #fff9;
+        margin: 10px;
+      }
+      textarea:focus {
+        border: 3px solid #fff;
+      }
     }
 
     div {
-        display: flex;
-        .email {
-            width: 50%;
-            span {
-                color: #fff;
-                margin-left: 10px;
-            }
-            
-            input {
-                outline: none;
-                height: 40px;
-                background-color: #fff0;
-                color: #fff;
-                border: 3px solid #fff9;
-                margin: 10px;
-            }
-            input:focus {
-                border: 3px solid #fff;
-            }
+      display: flex;
+      .email {
+        width: 50%;
+        span {
+          color: #fff;
+          margin-left: 10px;
         }
 
-        .nombre {
-            width: 50%;
-
-            span {
-                color: #fff;
-                margin-left: 10px;
-            }
-            
-            input {
-                outline: none;
-                height: 40px;
-                background-color: #fff0;
-                color: #fff;
-                border: 3px solid #fff9;
-                margin: 10px;
-            }
-            input:focus {
-                border: 3px solid #fff;
-            }
+        input {
+          outline: none;
+          height: 40px;
+          background-color: #fff0;
+          color: #fff;
+          border: 3px solid #fff9;
+          margin: 10px;
         }
+        input:focus {
+          border: 3px solid #fff;
+        }
+      }
+
+      .nombre {
+        width: 50%;
+
+        span {
+          color: #fff;
+          margin-left: 10px;
+        }
+
+        input {
+          outline: none;
+          height: 40px;
+          background-color: #fff0;
+          color: #fff;
+          border: 3px solid #fff9;
+          margin: 10px;
+        }
+        input:focus {
+          border: 3px solid #fff;
+        }
+      }
     }
-`;
+  }
+`
 
 const Contacto = () => {
+  return (
+    <Form id="CONTACTO" onSubmit={e => e.preventDefault()}>
+      <motion.div
+        className="divCont"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ delay: 0.4, duration: 1 }}
+      >
+        <div>
+          <label className="email">
+            <span>E-mail</span>
+            <input type="text" placeholder="E-mail" />
+          </label>
+          <label className="nombre">
+            <span>Nombre</span>
+            <input type="text" placeholder="Nombre" />
+          </label>
+        </div>
 
-    return (
-        <Form id="CONTACTO" onSubmit={e => e.preventDefault()}>
-            <div>
-                <label className="email">
-                    <span >E-mail</span>
-                    <input type='text' placeholder="E-mail"/>
-                </label>
-                <label className="nombre">
-                    <span>Nombre</span>
-                    <input  type='text' placeholder="Nombre"/>
-                </label>
-            </div>
-    
+        <label>
+          <textarea placeholder="Mensaje"></textarea>
+        </label>
 
-            <label>
-                <textarea placeholder="Mensaje">
-
-                </textarea>
-            </label>
-
-            <input className="sutmit" type='submit' value='Enviar' />
-        </Form>
-    )
+        <input className="sutmit" type="submit" value="Enviar" />
+      </motion.div>
+    </Form>
+  )
 }
 
 export default Contacto
